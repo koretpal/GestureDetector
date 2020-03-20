e099310d84e8306d74475b5434d731f8bb071f56
Excel <chukwunekwu.obioha@gmail.com>
2020-03-20 15:58:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/gesturedetector/Deuxa.java
@@ -0,0 +1,14 @@
+package com.example.gesturedetector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Deuxa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deuxa);
+    }
+}
---
+++ app/src/main/java/com/example/gesturedetector/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.gesturedetector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.MotionEvent;
+import android.view.GestureDetector;
+//import android.support.v4.view.GestureDetectorCompat;
+
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
+
+    private TextView myMessage;
+    private GestureDetector gestureDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myMessage = (TextView) findViewById(R.id.mymessage);
+        this.gestureDetector = new GestureDetector(this, this);
+        gestureDetector.setOnDoubleTapListener(this);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        myMessage.setText("SingleTapConfirmed");
+        startActivity(new Intent(MainActivity.this, Deuxa.class));
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        myMessage.setText("onDoubleTap");
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        myMessage.setText("onDoubleTapEvent");
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        myMessage.setText("onDown");
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        myMessage.setText("onShowPress");
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        myMessage.setText("onSingleTapUp");
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        myMessage.setText("onScroll");
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        myMessage.setText("onLongPress");
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        myMessage.setText("onFling");
+        return false;
+    }
+}
